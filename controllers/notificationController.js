5f28dd277fae4c332c18ac9ed8a7797385152bf7
Ceyhun Resulov <ceyhunresulov23@gmail.com>
2023-06-18 17:31:09 +0400

    change getNotificationsForAdmin

--- controllers/notificationController.js
+++ controllers/notificationController.js
@@ -60,9 +60,10 @@ export const createNotificationForUpdate = async (teacherId, students) => {
 export const getNotificationsForAdmin = async (req, res) => {
   try {
     const notifications = await Notification.find({
-      role: { $in: ["birhtday", "count"] },
+      role: { $in: ["birthday", "count"] },
     });
 
+    console.log(notifications);
     res.status(200).json(notifications);
   } catch (err) {
     res.status(500).json({ message: { error: err.message } });
