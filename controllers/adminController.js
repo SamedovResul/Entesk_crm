20cdc7594ff8b9b09dd9bdf08c2ee30149c24efc
Ceyhun Resulov <ceyhunresulov23@gmail.com>
2023-07-07 15:35:46 +0400

    update teacher uptadeAdminProfileImage

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -96,7 +96,14 @@ export const updateAdminImage = async (req, res) => {
       res.status(404).json({ message: "Not found admin" });
     }
 
-    res.status(200).json(updatedAdmin);
+    const base64Image = Buffer.from(updatedAdmin.profileImage).toString(
+      "base64"
+    );
+
+    const objAdmin = updatedAdmin.toObject();
+    objAdmin.profileImage = base64Image;
+
+    res.status(200).json(objAdmin);
   } catch (err) {
     res.status(500).json({ message: { error: err.message } });
   }
