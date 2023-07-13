22f1af721b2129e929d7c7e9d02455ab428b1924
Ceyhun Resulov <ceyhunresulov23@gmail.com>
2023-07-13 11:01:06 +0400

    update cors middleware

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ const uri = process.env.DB_URI;
 
 app.use(
   cors({
-    origin: "*",
+    origin: "http://localhost:3000",
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization", "Accept"],
   })
