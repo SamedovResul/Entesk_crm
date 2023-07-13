289f0cbc737b00b97fd157abe0574076ff27fcf3
Ceyhun Resulov <ceyhunresulov23@gmail.com>
2023-07-13 12:48:27 +0400

    add middleware profile image routes

--- routes/profileRoutes.js
+++ routes/profileRoutes.js
@@ -3,10 +3,11 @@ import {
   getProfileImage,
   uploadProfileImage,
 } from "../controllers/profileController.js";
+import { authMiddleware } from "../middleware/auth.js";
 
 const router = express.Router();
 
-router.get("/image", getProfileImage);
-router.patch("/image/upload", uploadProfileImage);
+router.get("/image", authMiddleware, getProfileImage);
+router.patch("/image/upload", authMiddleware, uploadProfileImage);
 
 export default router;
